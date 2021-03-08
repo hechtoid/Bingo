8129f459c439783ad3b06729ed74f6030436ab53
hechtoid <hechtsp@gmail.com>
2021-03-07 18:28:35 -0800

    remove old methods

--- frontend/src/components/app/button_bar.js
+++ frontend/src/components/app/button_bar.js
@@ -12,7 +12,7 @@ const msp = state => ({
 
 const mdp = dispatch => {
     return {
-        logout: () => {logout()(dispatch); closeUserModal()},
+        logout: () => {logout()(dispatch)},
     }
 }
 
