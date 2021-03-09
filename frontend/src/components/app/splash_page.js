127833225b2c9bc1a49c40c77f9722fc4a848b82
hechtoid <hechtsp@gmail.com>
2021-03-09 12:33:48 -0800

    convert splash page to functional component

--- frontend/src/components/app/splash_page.js
+++ frontend/src/components/app/splash_page.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import LoginFormContainer from '../session/login_form_container'
 
-class SplashPage extends React.Component {
-
-  render() {
-    return (
+const SplashPage = () => (
      <div className="splash">       
         <div className="splash-right-box">
           <LoginFormContainer/>
         </div>
-
      </div>
-    );
-  }
-}
+);
+
 
 export default SplashPage;
