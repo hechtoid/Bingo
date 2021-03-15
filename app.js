f0d8b48a3a6ce41f31f9a82a3a0d733dffd12bc1
hechtoid <hechtsp@gmail.com>
2021-03-14 23:48:28 -0700

    set up front end for deployment

--- app.js
+++ app.js
@@ -7,20 +7,11 @@ const passport = require('passport');
 const path = require('path');
 const users = require("./routes/api/users");
 const wordlists = require("./routes/api/wordlists");
-console.log(process.env);
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    })
-}
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-
-app.get("/", (req, res) => res.send("Hello World!!"));
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
@@ -30,6 +21,11 @@ app.use(bodyParser.json());
 
 app.use("/api/users", users);
 app.use("/api/wordlists", wordlists);
+
+app.use(express.static('frontend/build'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
